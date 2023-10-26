2007f758700ea4da23ee7e6e51d05e5f66727438
excelsiorvita <lesbuble0707@gmail.com>
2023-10-26 15:50:31 +0300

    Tour of Heroes, DI

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
+import { HeroService } from '../hero.service';
+import { MessagesService } from '../messages.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+  selectedHero?: Hero;
+  heroes:Hero[] = [];
+
+  constructor(private heroService: HeroService, private messagesService: MessagesService){}
+
+  ngOnInit():void{
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero):void {
+    this.selectedHero = hero;
+    this.messagesService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+
+}
